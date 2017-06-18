344e53a94baf7e1accadf3323f24cdfd6f297db1
zhouyuhuan <1120726720@qq.com>
2017-06-18 16:16:14 +0800

    添加登录测试

--- src/main/java/com/zes/squad/gmh/web/entity/dto/StaffDto.java
+++ src/main/java/com/zes/squad/gmh/web/entity/dto/StaffDto.java
@@ -1,5 +1,16 @@
 package com.zes.squad.gmh.web.entity.dto;
 
+import lombok.Data;
+
+@Data
 public class StaffDto {
+
+    private Long   id;
+
+    private String email;
+
+    private String password;
+
+    private String salt;
 
 }
--- src/main/java/com/zes/squad/gmh/web/entity/vo/StaffVo.java
+++ src/main/java/com/zes/squad/gmh/web/entity/vo/StaffVo.java
@@ -1,5 +1,16 @@
 package com.zes.squad.gmh.web.entity.vo;
 
+import lombok.Data;
+
+@Data
 public class StaffVo {
+
+    private Long   id;
+
+    private String email;
+
+    private String password;
+
+    private String salt;
 
 }
